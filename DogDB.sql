c6d8262b11665b782c5a2ef98d6d6332b79fd0a4
Branko Sudimac <49121413+Bane85@users.noreply.github.com>
2019-09-24 17:07:50 +0200

    Add files via upload

--- DogDB.sql
+++ DogDB.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 11, 2019 at 11:20 AM
+-- Generation Time: Sep 14, 2019 at 01:13 PM
 -- Server version: 5.6.37
 -- PHP Version: 5.6.31
 
@@ -23,14 +23,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(128) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='admini';
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pets`
 --
 
 CREATE TABLE IF NOT EXISTS `pets` (
-  `dog_name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `dog_breed` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `dog_age` int(11) NOT NULL,
-  `dog_color` varchar(32) COLLATE utf8_unicode_ci NOT NULL
+  `id` int(11) NOT NULL,
+  `dog_name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `dog_breed` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `dog_age` tinyint(4) NOT NULL,
+  `dog_color` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `dog_description` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='data about dogs';
 
 -- --------------------------------------------------------
@@ -40,12 +54,16 @@ CREATE TABLE IF NOT EXISTS `pets` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `ordinal_number` int(11) NOT NULL,
-  `username` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `mail` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-  `mobile_phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `city` varchar(15) COLLATE utf8_unicode_ci NOT NULL
+  `id` int(11) NOT NULL,
+  `first_name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `second_name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `mail` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `date_of_birth` int(11) NOT NULL,
+  `phone` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `city` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `date_of_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='podaci o korisniku';
 
 --
@@ -53,37 +71,52 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `pets`
 --
 ALTER TABLE `pets`
-  ADD PRIMARY KEY (`dog_name`),
-  ADD KEY `dog_name` (`dog_name`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`ordinal_number`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`first_name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pets`
+--
+ALTER TABLE `pets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ordinal_number` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `pets`
+-- Constraints for table `users`
 --
-ALTER TABLE `pets`
-  ADD CONSTRAINT `user_pet` FOREIGN KEY (`dog_name`) REFERENCES `users` (`username`);
+ALTER TABLE `users`
+  ADD CONSTRAINT `user_pet` FOREIGN KEY (`id`) REFERENCES `pets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
