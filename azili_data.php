11cb7fb2a73ea1b207965c371a7696384a69ac7a
Dalibor Kalember <49121432+Kalember@users.noreply.github.com>
2019-10-03 20:41:29 +0200

    azili - konekcija ka bazi
    test

---
+++ azili_data.php
@@ -0,0 +1,20 @@
+<?php
+include ('connect.php');
+
+$query = "SELECT * FROM azili ORDER BY id DESC";
+$result = mysqli_query($connection, $query) or die("SQL Error 1: " . mysqli_error($connection));
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    $azili[] = array(
+        'naziv' => $row['naziv'],
+        'mesto' => $row['mesto'],
+        'adresa' => $row['adresa'],
+        'telefon' => $row['telefon'],
+        'email' => $row['email'],
+        'slika' => $row['slika'],
+        'opis' => $row['opis']
+
+      );
+}
+ 
+echo json_encode($azili);
+?>
