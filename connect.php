967602e6ef7c5454521f0fd5e522a1f9842c2e60
Dalibor Kalember <49121432+Kalember@users.noreply.github.com>
2019-10-17 23:34:21 +0200

    prebaceno u php folder

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connection = new mysqli('localhost', 'root', '', 'dogdb');
-
-if ($connection->connect_error) { // Check connection
- die("Connection failed: " . $connection->connect_error);
-} 
-
-
-?>
