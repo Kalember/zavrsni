73349c327ad4639befe7d8c28b2ac45401d49ea8
Branko Sudimac <49121413+Bane85@users.noreply.github.com>
2019-10-22 23:57:48 +0200

    Dugme - ženska imena

---
+++ php/get_Fnames.php
@@ -0,0 +1,19 @@
+<?php
+include ('connect.php');
+
+$query = "SELECT * FROM dog_names where gender = 'f' ORDER BY rand() limit 1";
+$result = mysqli_query($connection, $query) or die("SQL Error 1: " . mysqli_error($connection));
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    $imena[] = array(
+        'ime' => $row['names'],
+        'pol' => $row['gender'],
+        'slovo' => $row['fletter'],
+        
+      );
+      $random_keys=array_rand($imena,1);
+}
+ 
+echo json_encode($imena);
+
+
+?>
